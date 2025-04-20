b3a5c103db7d6d4892db439c5a9bf42297294ffc
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-20 22:13:49 +0300

    Importing error and spinner components

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// import "./src/components/Error.js";
+import "./src/components/Error.js";
 import "./src/components/Joblist.js";
 import "./src/components/Search.js";
-// import "./src/components/Sorting.js";
+import "./src/components/Spinner.js";
 
 
 
