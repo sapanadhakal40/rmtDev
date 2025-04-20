be74169ad6e62df8be99ddb11739e3c90c53301c
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-20 20:45:12 +0300

    Export all DOM element selections

---
+++ src/common.js
@@ -0,0 +1,21 @@
+//global---
+export const bookmarksBtnElement = document.querySelector('.bookmarks-btn');
+export const errorElement = document.querySelector('.error');
+export const errorTextElement = document.querySelector('.error__text');
+export const jobDetailsElement = document.querySelector('.job-details');
+export const jobDetailsContentElement = document.querySelector(".job-details__content");
+export const jobListBookmarksElement = document.querySelector('.job-list--bookmarks');
+export const jobListSearchElement = document.querySelector(".job-list--search");
+export const numberElement = document.querySelector(".count__number");
+export const paginationElement = document.querySelector(".pagination");
+export const paginationBtnNextElement = document.querySelector(".pagination__button--next");
+export const paginationBtnBackElement = document.querySelector(".pagination__button--back");
+export const paginationNumberNextElement = document.querySelector(".pagination__number--next");
+export const paginationNumberBackElement = document.querySelector(".pagination__number--back");
+export const searchFormElement = document.querySelector(".search");
+export const searchInputElement = document.querySelector(".search__input");
+export const sortingElement = document.querySelector(".sorting");
+export const sortingBtnRelevantElement = document.querySelector(".sorting__button--relevant");
+export const sortingBtnRecentElement = document.querySelector(".sorting__button--recent");
+export const spinnerSearchElement = document.querySelector(".spinner--search");
+export const spinnerJobDetailsElement = document.querySelector(".spinner--job-details");
