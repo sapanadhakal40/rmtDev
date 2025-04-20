564de7dfebe952d8440058774a5ed67442893603
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-21 01:29:16 +0300

    Update index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "./src/components/Error.js";
 import "./src/components/Joblist.js";
 import "./src/components/Search.js";
 import "./src/components/Spinner.js";
+import "./src/components/Sorting.js";
 
 
 
